84b34723779beaa460b09aca71234c6427d50595
Linh Huynh <linh.huynh@mths.ca>
2025-03-25 09:20:01 -0400

    Update code.c

--- code.c
+++ code.c
@@ -2,26 +2,26 @@
 // created on: mar 2025
 
 const int trigPin = 9;
-const int echoPin = 8;
+const int echoPin = 10;
 
 float duration, distance;
 
 void setup() {
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
-    Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-    duration = pulseIn(echoPin, HIGH);
-    distance = (duration*.0343)/2;
-    Serial.print("Distance: ");
-    Serial.println(distance);
-    delay(100);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
 }
